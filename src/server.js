f9f4e97bc3ecda759985a957691f4adf1db1843e
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-14 20:13:47 -0300

    added GET users method

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import http from 'node:http'
 
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@example.com'
+  }
+]
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello world')
+const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .end(JSON.stringify(users))
+  }
+
+  return res.end()
 })
 
 server.listen(3333)

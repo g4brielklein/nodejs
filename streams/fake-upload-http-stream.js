de6ee8a16d2468ebb3ed9ec94f53ebb8dec4d321
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-16 23:16:18 -0300

    refactored http server and stream

--- streams/fake-upload-http-stream.js
+++ streams/fake-upload-http-stream.js
@@ -1,23 +1,27 @@
 import { Readable } from 'node:stream'
 
-class fakeUploadStream extends Readable {
-  index = 1
+class SendIdsStream extends Readable {
+  idIndex = 1
 
   _read() {
-    const i = this.index++
-
+    const index = this.idIndex++;
+    
     setTimeout(() => {
-      if (i > 100) {
+      if (index > 100) {
         this.push(null)
       } else {
-        this.push(Buffer.from(String(i)))
+        this.push(Buffer.from(String(index)))
       }
     }, 10)
-  }
+  }  
 }
 
 fetch('http://localhost:3333', {
   method: 'POST',
-  body: new fakeUploadStream(),
+  body: new SendIdsStream(),
   duplex: 'half',
+}).then(response => {
+  return response.text()
+}).then(data => {
+  console.log(data)
 })
--- streams/server-stream.js
+++ streams/server-stream.js
@@ -1,17 +1,25 @@
 import http from 'node:http'
 import { Transform } from 'node:stream'
 
-class getFakeUploadStream extends Transform {
+class CreateUniqueNameBasedOnId extends Transform {
   _transform(chunk, encoding, callback) {
-    console.log(String(chunk))
-    callback(null, Buffer.from(String(chunk)))
+    const transformed = `item-${Number(chunk)}-identifier`
+    console.log(transformed)
+
+    callback(null, Buffer.from(transformed))
   }
 }
 
-const server = http.createServer((req, res) => {
-  return req
-    .pipe(new getFakeUploadStream())
-    .pipe(res)
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+  
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  return res.end(fullStreamContent)
 })
 
 server.listen(3333)

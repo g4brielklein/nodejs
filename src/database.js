379e70f947ef348f850cda428efa8cebdfd0f973
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-18 19:43:53 -0300

    added `node fs` to save data on .json file

--- src/database.js
+++ src/database.js
@@ -1,15 +1,40 @@
+import fs from 'node:fs/promises'
+const filePath = new URL('data.json', import.meta.url)
+
 export class Database {
   #database = {}
 
+  constructor() {
+    fs.readFile(filePath)
+      .then(file => {
+        this.#database = JSON.parse(file)
+      })
+      .catch(() => {
+        this.#saveOnFile()
+      })
+  }
+
+  #saveOnFile() {
+    fs.writeFile(filePath, JSON.stringify(this.#database, null, 2))
+    .then(() => {
+      console.log('File writen successfully')
+    })
+    .catch(err => {
+      console.log('Error writing file', err)
+    })
+  }
+  
   select(table) {
     return this.#database[table] ?? []
   }
-
+  
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      return this.#database[table].push(data)
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
     }
-
-    this.#database[table] = [data]
+    
+    this.#saveOnFile()
   }
 }

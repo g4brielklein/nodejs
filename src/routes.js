7ed2bfe61ce16748370792005a0a1c75acac7870
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-20 21:12:40 -0300

    refactored routes.js adding `targetTable` var

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,8 @@ export const routes = [
     method: 'GET',
     url: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users')
+      const targetTable = 'users'
+      const users = database.select(targetTable)
 
       return res.end(JSON.stringify(users))
     }
@@ -19,6 +20,7 @@ export const routes = [
     url: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
+      const targetTable = 'users'
 
       const user = {
         id: randomUUID(),
@@ -26,7 +28,7 @@ export const routes = [
         email,
       }
 
-      database.insert('users', user)
+      database.insert(targetTable, user)
       
       return res.writeHead(201).end()
     }
@@ -40,8 +42,9 @@ export const routes = [
         name: req.body?.name,
         email: req.body?.email,
       }
+      const targetTable = 'users'
 
-      database.update('users', data)
+      database.update(targetTable, data)
 
       return res.writeHead(204).end()
     }
@@ -51,7 +54,9 @@ export const routes = [
     url: buildRoutePath('/users/:id'),
     handler: (req, res) => {
       const { id } = req.params
-      database.delete('users', id)
+      const targetTable = 'users'
+
+      database.delete(targetTable, id)
       
       return res.writeHead(204).end()
     }

8435a4d2b684624c266052f211af59e7a70f0e40
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-21 20:24:26 -0300

    adjusted regex to include query params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,7 +2,7 @@ export function buildRoutePath(url) {
   const routeParamsRegex = /:([a-zA-Z]+)/g
   const pathWithParams = url.replaceAll(routeParamsRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }

de898f679c979af303028fb89379d3e28bf932ab
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-22 23:55:14 -0300

    fixed `undefined` search term

--- src/utils/extract-query-params.js
+++ src/utils/extract-query-params.js
@@ -1,7 +1,9 @@
 export function extractQueryParams(query) {
   return query.substr(1).split('&').reduce((queryParams, param) => {
     const [key, value] = param.split('=')
+
     queryParams[key] = decodeURIComponent(value)
+    queryParams[key] = queryParams[key] === 'undefined' ? null : queryParams[key]
 
     return queryParams
   }, {})
